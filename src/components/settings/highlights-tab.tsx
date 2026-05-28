80d927669a4f07e0715dccff1db454772f06903f
Riley Wilson <riley@rcw.lol>
2026-05-28 12:48:46 -0400

    Updater highlight tab to show WIP

--- src/components/settings/highlights-tab.tsx
+++ src/components/settings/highlights-tab.tsx
@@ -1,10 +1,22 @@
-import { SettingsTab } from "@/components/settings/settings-primitives"
+import { SettingsTab, SettingsDivider, SettingsCallout } from "@/components/settings/settings-primitives"
 
 export function HighlightsTab() {
   return (
     <SettingsTab
       title="Highlights"
       description="Highlight rules and visual emphasis for messages that matter to you."
-    />
-  )
+    >
+
+    <SettingsDivider />
+
+    <SettingsCallout title="Not implemented yet">
+      This feature has not been implemented yet. In the future, it'll allow you to:
+      <ul className="list-disc list-inside">
+        <li>Get notified when a message matches a highlight rule.</li>
+        <li>See when a chat has unread messages when not in view.</li>
+        <li>Get notified when a channel goes live.</li>
+      </ul>
+    </SettingsCallout>
+  </SettingsTab>
+  );
 }
